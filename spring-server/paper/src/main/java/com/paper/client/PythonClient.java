77f20c61061d3761e0f3bf0e3388d82464d23c8e
dkqpeo <lan0184@naver.com>
2025-10-30 17:20:55 +0900

    feature : 백엔드 서버에서 파이썬 api호출 구현.

--- spring-server/paper/src/main/java/com/paper/client/PythonClient.java
+++ spring-server/paper/src/main/java/com/paper/client/PythonClient.java
@@ -1,4 +1,39 @@
 package com.paper.client;
 
+import com.paper.dto.TestResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class PythonClient {
+
+    private final WebClient pythonWebClient;
+
+    /**
+     * 테스트 api 호출
+     */
+    public Mono<TestResponse> test () {
+
+        log.info("PythonClient test");
+
+        return pythonWebClient.get()
+                .uri("/")
+                .retrieve()
+                .bodyToMono(TestResponse.class)
+                .timeout(Duration.ofSeconds(3))
+                .doOnSuccess(resp ->
+                        log.info("Call Test Python API response received!")
+                )
+                .doOnError(err ->
+                        log.error("Call Test Python API Failed {}", err.getMessage())
+                );
+
+    }
 }
--- spring-server/paper/src/main/java/com/paper/config/WebClientConfig.java
+++ spring-server/paper/src/main/java/com/paper/config/WebClientConfig.java
@@ -1,4 +1,23 @@
 package com.paper.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    @Value("${python.ai-service.url}")
+    private String aiServiceUrl;
+
+    @Bean
+    public WebClient pythonWebClient() {
+        return WebClient.builder()
+                .baseUrl(aiServiceUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
 }
